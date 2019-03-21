0f9c2b68748fa68309a95f279f0e841c0adaa13f
David Lucero <david.lucero@ttu.edu>
2019-03-21 03:04:03 -0500

    commit

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var request = require('request')
 app.use('/',express.static(__dirname + '/'));
 app.use("/assets", express.static(__dirname + "/assets"));
 
-
-
 app.get('/', function (req, res) {
 
     res.send(path.join(__dirname + '/index.html'));
--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var schedule = require('node-schedule')
 var request = require('request');
 
+
 function foo(){
     var rule = new schedule.RecurrenceRule();
-    rule.hour = 2;
-    
+    rule.hour = 7;
+   
     let quotep = document.querySelector("#quote")
-
+    let quoteauthor = document.querySelector("#quoteauthor");
     var j = schedule.scheduleJob(rule, function(){
         console.log("new quote!!");
         request('http://quotes.rest/qod.json', function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            var quote = JSON.parse(body).contents.quotes[0].quote
-            quotep.innerHTML = quote;
-    
+            var quote = JSON.parse(body).contents.quotes[0]
+            quotep.innerHTML = quote.quote;
+            quoteauthor.innerHTML = quote.author
         });
     })
 }
 foo();
 
-var fishbtn = document.querySelector("#fishbtn");
-fishbtn.addEventListener("click", function(){
-    console.log("fish");
-    
+
+let emailbtn = document.querySelector("#emailbtn")
+emailbtn.addEventListener("click", function(){
+    console.log("email");
+
+
    
-    
+   
 })
-function fish(){
-    console.log("fish");
-    
-}
+
