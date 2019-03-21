f5502af9923b310734f85964a5e650d528da6792
David Lucero <david.lucero@ttu.edu>
2019-03-21 12:46:42 -0500

    commit

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ function foo(){
 }
 foo();
 
+window.onload = function(){
+    console.log("loaded")
+    let quotep = document.querySelector("#quote")
+    let quoteauthor = document.querySelector("#quoteauthor");
+    request('http://quotes.rest/qod.json', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        var quote = JSON.parse(body).contents.quotes[0]
+        quotep.innerHTML = quote.quote;
+        quoteauthor.innerHTML = quote.author
+    });
+}
 
 let emailbtn = document.querySelector("#emailbtn")
 emailbtn.addEventListener("click", function(){
